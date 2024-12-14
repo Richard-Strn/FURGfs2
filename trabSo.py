4516688bc8ce476840f70dff88566596729e3054
Richard-Strn <richardtkkz@gmail.com>
2024-12-14 16:49:49 -0300

    renomeacao

--- trabSo.py
+++ trabSo.py
@@ -1,223 +1,221 @@
 import os
 import struct
 
-class SimpleFATFileSystem:
-    BLOCK_SIZE = 4096
-    MAX_FILE_NAME_LENGTH = 255 
+class FURGfs2:
 
-    def __init__(self, file_name, total_size):
-        self.file_name = file_name
-        self.total_size = total_size
-        self.total_blocks = total_size // self.BLOCK_SIZE
-        self.header_size = 512
-        self.fat_start = self.header_size
-        self.fat_size = self.total_blocks * 4 
-        self.root_dir_start = self.fat_start + self.fat_size
-        self.data_start = self.root_dir_start + self.BLOCK_SIZE 
-        self.fat = [-1] * self.total_blocks
-        self.root_dir = {} 
-        self.init_file()
+    def __init__(self, tamanhoTotal):
+        self.nomeSistemaArquivos = "FURGSfs2.fs"
+        self.tamanhoBloco = 4096
+        self.tamanhoMaximoNomeArquivo = 255
+        self.tamanhoTotal = tamanhoTotal
+        self.qtdTotalBlocos = tamanhoTotal // self.tamanhoBloco
+        self.tamanhoHeader = 512
+        self.inicioFat = self.tamanhoHeader
+        self.tamanhoFat = self.qtdTotalBlocos * 4 
+        self.inicioRoot = self.inicioFat + self.tamanhoFat
+        self.inicioData = self.inicioRoot + self.tamanhoBloco 
+        self.fat = [-1] * self.qtdTotalBlocos
+        self.root = {} 
+        self.iniciarFURGfs2()
 
-    def init_file(self):
-        if not os.path.exists(self.file_name):
-            with open(self.file_name, 'wb') as f:
-                f.write(b'\x00' * self.total_size)
-            self.write_header()
-            self.save_fat()
+    def iniciarFURGfs2(self):
+        if not os.path.exists(self.nomeSistemaArquivos):
+            with open(self.nomeSistemaArquivos, 'wb') as sistemaArquivos:
+                sistemaArquivos.write(b'\x00' * self.tamanhoTotal)
+            self.escreverCabecalho()
+            self.salvarFat()
 
-    def write_header(self):
-        with open(self.file_name, 'r+b') as f:
-            header_data = struct.pack(
+    def escreverCabecalho(self):
+        with open(self.nomeSistemaArquivos, 'r+b') as sistemaArquivos:
+            dadosCabecalho = struct.pack(
                 'I I I I I I',
-                self.header_size,   
-                self.BLOCK_SIZE,   
-                self.total_size,   
-                self.fat_start,
-                self.root_dir_start, 
-                self.data_start   
+                self.tamanhoHeader,   
+                self.tamanhoBloco,   
+                self.tamanhoTotal,   
+                self.inicioFat,
+                self.inicioRoot, 
+                self.inicioData   
             )
-            f.write(header_data.ljust(self.header_size, b'\x00'))
+            sistemaArquivos.write(dadosCabecalho.ljust(self.tamanhoHeader, b'\x00'))
 
-    def save_fat(self):
-        with open(self.file_name, 'r+b') as f:
-            f.seek(self.fat_start)
-            fat_data = struct.pack(f"{self.total_blocks}i", *self.fat)
-            f.write(fat_data)
+    def salvarFat(self):
+        with open(self.nomeSistemaArquivos, 'r+b') as sistemaArquivos:
+            sistemaArquivos.seek(self.inicioFat)
+            dadosFat = struct.pack(f"{self.qtdTotalBlocos}i", *self.fat)
+            sistemaArquivos.write(dadosFat)
 
-    def load_fat(self):
-        with open(self.file_name, 'rb') as f:
-            f.seek(self.fat_start)
-            fat_data = f.read(self.total_blocks * 4)
-            self.fat = list(struct.unpack(f"{self.total_blocks}i", fat_data))
+    def carregarFat(self):
+        with open(self.nomeSistemaArquivos, 'rb') as sistemaArquivos:
+            sistemaArquivos.seek(self.inicioFat)
+            dadosFat = sistemaArquivos.read(self.qtdTotalBlocos * 4)
+            self.fat = list(struct.unpack(f"{self.qtdTotalBlocos}i", dadosFat))
 
-    def save_root_dir(self):
-        with open(self.file_name, 'r+b') as f:
-            f.seek(self.root_dir_start)
-            root_data = str(self.root_dir).encode().ljust(self.BLOCK_SIZE, b'\x00')
-            f.write(root_data)
+    def salvarRoot(self):
+        with open(self.nomeSistemaArquivos, 'r+b') as sistemaArquivos:
+            sistemaArquivos.seek(self.inicioRoot)
+            dadosRoot = str(self.root).encode().ljust(self.tamanhoBloco, b'\x00')
+            sistemaArquivos.write(dadosRoot)
 
-    def load_root_dir(self):
-        with open(self.file_name, 'rb') as f:
-            f.seek(self.root_dir_start)
-            root_data = f.read(self.BLOCK_SIZE).decode().strip('\x00')
-            self.root_dir = eval(root_data) if root_data else {}
+    def carregarRoot(self):
+        with open(self.nomeSistemaArquivos, 'rb') as sistemaArquivos:
+            sistemaArquivos.seek(self.inicioRoot)
+            dadosRoot = sistemaArquivos.read(self.tamanhoBloco).decode().strip('\x00')
+            self.root = eval(dadosRoot) if dadosRoot else {}
 
-    def find_free_blocks(self, num_blocks):
+    def acharBlocosDisponiveis(self, blocosNecessarios):
         """
         Encontra e retorna uma lista de blocos livres no sistema de arquivos.
 
-        :param num_blocks: Número de blocos necessários.
+        :param blocosNecessarios: Número de blocos necessários.
         :return: Lista de blocos livres ou None se não houver blocos suficientes.
         """
-        free_blocks = [i for i, block in enumerate(self.fat) if block == -1]
-        if len(free_blocks) >= num_blocks:
-            return free_blocks[:num_blocks]
+        blocosDisponiveis = [i for i, bloco in enumerate(self.fat) if bloco == -1]
+        if len(blocosDisponiveis) >= blocosNecessarios:
+            return blocosDisponiveis[:blocosNecessarios]
         return None
 
-    def copy_to_fs(self, source_path):
+    def copiarArquivoDiscoSistema(self, enderecoArquivoDisco):
         """
         Copia um arquivo do disco para o sistema de arquivos.
         
         :param source_path: Caminho do arquivo no disco.
         """
-        file_name = os.path.basename(source_path)
-        if len(file_name) > self.MAX_FILE_NAME_LENGTH:
-            raise ValueError(f"O nome do arquivo excede o tamanho máximo de {self.MAX_FILE_NAME_LENGTH} caracteres.")
+        nomeArquivoDisco = os.path.basename(enderecoArquivoDisco)
+        if len(nomeArquivoDisco) > self.tamanhoMaximoNomeArquivo:
+            raise ValueError(f"O nome do arquivo excede o tamanho máximo de {self.tamanhoMaximoNomeArquivo} caracteres.")
 
-        with open(source_path, 'rb') as f:
-            data = f.read()
+        with open(enderecoArquivoDisco, 'rb') as sistemaArquivos:
+            arquivoDisco = sistemaArquivos.read()
         
-        print(len(data))
+        print(len(arquivoDisco))
 
-        num_blocks = (len(data) + self.BLOCK_SIZE - 1) // self.BLOCK_SIZE
-        print(num_blocks)  
-        free_blocks = self.find_free_blocks(num_blocks)
+        blocosNecessarios = (len(arquivoDisco) + self.tamanhoBloco - 1) // self.tamanhoBloco
+        print(blocosNecessarios)  
+        blocosDisponiveis = self.acharBlocosDisponiveis(blocosNecessarios)
 
-        if not free_blocks:
-            raise Exception(f"Espaço insuficiente no sistema de arquivos para armazenar o arquivo '{file_name}'.")
+        if not blocosDisponiveis:
+            raise Exception(f"Espaço insuficiente no sistema de arquivos para armazenar o arquivo '{nomeArquivoDisco}'.")
 
-        self.load_fat()
-        self.load_root_dir()
+        self.carregarFat()
+        self.carregarRoot()
 
-        start_block = free_blocks[0]
-        self.root_dir[file_name] = {
-            "start_block": start_block,
-            "size": len(data),
-            "protected": False
+        inicioBloco = blocosDisponiveis[0]
+        self.root[nomeArquivoDisco] = {
+            "inicioBloco": inicioBloco,
+            "tamanho": len(arquivoDisco),
+            "protegido": False
         }
 
-        with open(self.file_name, 'r+b') as f:
-            for i, block in enumerate(free_blocks):
-                start = i * self.BLOCK_SIZE
-                end = start + self.BLOCK_SIZE
-                f.seek(self.data_start + self.BLOCK_SIZE * block)
-                f.write(data[start:end].ljust(self.BLOCK_SIZE, b'\x00'))
+        with open(self.nomeSistemaArquivos, 'r+b') as sistemaArquivos:
+            for i, bloco in enumerate(blocosDisponiveis):
+                start = i * self.tamanhoBloco
+                end = start + self.tamanhoBloco
+                sistemaArquivos.seek(self.inicioData + self.tamanhoBloco * bloco)
+                sistemaArquivos.write(arquivoDisco[start:end].ljust(self.tamanhoBloco, b'\x00'))
 
-                self.fat[block] = free_blocks[i + 1] if i + 1 < len(free_blocks) else -2
+                self.fat[bloco] = blocosDisponiveis[i + 1] if i + 1 < len(blocosDisponiveis) else -2
 
-        self.save_fat()
-        self.save_root_dir()
-        print(f"Arquivo '{file_name}' copiado para o sistema de arquivos.")
+        self.salvarFat()
+        self.salvarRoot()
+        print(f"Arquivo '{nomeArquivoDisco}' copiado para o sistema de arquivos.")
 
-    def copy_from_fs(self, file_name, dest_path):
-        self.load_root_dir()
-        if file_name not in self.root_dir:
-            raise FileNotFoundError(f"Arquivo '{file_name}' não encontrado no sistema de arquivos.")
+    def copiarArquivoSistemaDisco(self, nomeArquivoSistema, enderecoDestino):
+        self.carregarRoot()
+        if nomeArquivoSistema not in self.root:
+            raise FileNotFoundError(f"Arquivo '{nomeArquivoSistema}' não encontrado no sistema de arquivos.")
 
-        file_info = self.root_dir[file_name]
-        start_block = file_info["start_block"]
-        size = file_info["size"]
+        dadosArquivoSistema = self.root[nomeArquivoSistema]
+        inicioBloco = dadosArquivoSistema["inicioBloco"]
+        tamanho = dadosArquivoSistema["tamanho"]
 
         data = b''
 
-        self.load_fat()
-        with open(self.file_name, 'rb') as f:
-            block = start_block
-            while block != -2:
-                f.seek(self.data_start + self.BLOCK_SIZE * block)
-                data += f.read(self.BLOCK_SIZE)
-                block = self.fat[block]  
+        self.carregarFat()
+        with open(self.nomeSistemaArquivos, 'rb') as sistemaArquivos:
+            bloco = inicioBloco
+            while bloco != -2:
+                sistemaArquivos.seek(self.inicioData + self.tamanhoBloco * bloco)
+                data += sistemaArquivos.read(self.tamanhoBloco)
+                bloco = self.fat[bloco]  
 
-        if not os.path.isdir(dest_path):
-            raise NotADirectoryError(f"O caminho '{dest_path}' não é um diretório válido.")
+        if not os.path.isdir(enderecoDestino):
+            raise NotADirectoryError(f"O caminho '{enderecoDestino}' não é um diretório válido.")
 
-        dest_file_path = os.path.join(dest_path, file_name)
+        enderecoArquivoDisco = os.path.join(enderecoDestino, nomeArquivoSistema)
     
-        with open(dest_file_path, 'wb') as f:
-            f.write(data[:size])
+        with open(enderecoArquivoDisco, 'wb') as sistemaArquivos:
+            sistemaArquivos.write(data[:tamanho])
 
-        print(f"Arquivo '{file_name}' copiado para '{dest_file_path}'.")
-
-
-    def rename_file(self, old_name, new_name):
-        self.load_root_dir()
-        if old_name not in self.root_dir:
-            raise FileNotFoundError(f"Arquivo '{old_name}' não encontrado no sistema de arquivos.")
-
-        if len(new_name) > self.MAX_FILE_NAME_LENGTH:
-            raise ValueError(f"O nome do arquivo excede o tamanho máximo de {self.MAX_FILE_NAME_LENGTH} caracteres.")
-
-        self.root_dir[new_name] = self.root_dir.pop(old_name)
-        self.save_root_dir()
-        print(f"Arquivo '{old_name}' renomeado para '{new_name}'.")
-
-    def remove_file(self, file_name):
-        self.load_root_dir()
-        if file_name not in self.root_dir:
-            raise FileNotFoundError(f"Arquivo '{file_name}' não encontrado no sistema de arquivos.")
-
-        file_info = self.root_dir[file_name]
-        start_block = file_info["start_block"]
-
-        self.load_fat()
-        block = start_block
-        while block != -2:
-            next_block = self.fat[block]
-            self.fat[block] = -1
-            block = next_block
-
-        del self.root_dir[file_name]
-        self.save_fat()
-        self.save_root_dir()
-        print(f"Arquivo '{file_name}' removido do sistema de arquivos.")
-
-    def list_files(self):
-        self.load_root_dir()
-        for file_name, file_info in self.root_dir.items():
-            status = "protegido" if file_info["protected"] else "normal"
-            print(f"{file_name} - {file_info['size']} bytes - {status}")
-
-    def get_free_spacee(self):
-        free_blocks = self.fat.count(-1)
-        free_space = free_blocks * self.BLOCK_SIZE
-        total_space = self.total_blocks * self.BLOCK_SIZE
-        free_space_mb = free_space / (1024 * 1024)
-        total_space_mb = total_space / (1024 * 1024)
-        print(f"{free_space_mb:.1f} MB livres de {total_space_mb:.1f} MB.")
+        print(f"Arquivo '{nomeArquivoSistema}' copiado para '{enderecoArquivoDisco}'.")
 
 
-    def protect_file(self, file_name, protect=True):
-        self.load_root_dir()
-        if file_name not in self.root_dir:
-            raise FileNotFoundError(f"Arquivo '{file_name}' não encontrado no sistema de arquivos.")
+    def renomearArquivo(self, nomeAntigo, nomeNovo):
+        self.carregarRoot()
+        if nomeAntigo not in self.root:
+            raise FileNotFoundError(f"Arquivo '{nomeAntigo}' não encontrado no sistema de arquivos.")
 
-        self.root_dir[file_name]["protected"] = protect
-        self.save_root_dir()
-        action = "protegido" if protect else "desprotegido"
-        print(f"Arquivo '{file_name}' {action} com sucesso.")
+        if len(nomeNovo) > self.tamanhoMaximoNomeArquivo:
+            raise ValueError(f"O nome do arquivo excede o tamanho máximo de {self.tamanhoMaximoNomeArquivo} caracteres.")
+
+        self.root[nomeNovo] = self.root.pop(nomeAntigo)
+        self.salvarRoot()
+        print(f"Arquivo '{nomeAntigo}' renomeado para '{nomeNovo}'.")
+
+    def deletarArquivo(self, nomeArquivoSistema):
+        self.carregarRoot()
+        if nomeArquivoSistema not in self.root:
+            raise FileNotFoundError(f"Arquivo '{nomeArquivoSistema}' não encontrado no sistema de arquivos.")
+
+        dadosArquivoSistema = self.root[nomeArquivoSistema]
+        inicioBloco = dadosArquivoSistema["inicioBloco"]
+
+        self.carregarFat()
+        bloco = inicioBloco
+        while bloco != -2:
+            proximoBloco = self.fat[bloco]
+            self.fat[bloco] = -1
+            bloco = proximoBloco
+
+        del self.root[nomeArquivoSistema]
+        self.salvarFat()
+        self.salvarRoot()
+        print(f"Arquivo '{nomeArquivoSistema}' removido do sistema de arquivos.")
+
+    def informarArquivosSistema(self):
+        self.carregarRoot()
+        for nomeArquivoSistema, dadosArquivoSistema in self.root.items():
+            status = "protegido" if dadosArquivoSistema["protegido"] else "normal"
+            print(f"{nomeArquivoSistema} - {dadosArquivoSistema['tamanho']} bytes - {status}")
+
+    def informarEspacoDisponivel(self):
+        blocosDisponiveis = self.fat.count(-1)
+        espacoDisponivel = blocosDisponiveis * self.tamanhoBloco
+        espacoTotal = self.qtdTotalBlocos * self.tamanhoBloco
+        espacoDisponivelMb = espacoDisponivel / (1024 * 1024)
+        espacoTotalMb = espacoTotal / (1024 * 1024)
+        print(f"{espacoDisponivelMb:.1f} MB livres de {espacoTotalMb:.1f} MB.")
+
+
+    def protegerArquivo(self, nomeArquivoSistema, protegido=True):
+        self.carregarRoot()
+        if nomeArquivoSistema not in self.root:
+            raise FileNotFoundError(f"Arquivo '{nomeArquivoSistema}' não encontrado no sistema de arquivos.")
+
+        self.root[nomeArquivoSistema]["protegido"] = protegido
+        self.salvarRoot()
+        action = "protegido" if protegido else "desprotegido"
+        print(f"Arquivo '{nomeArquivoSistema}' {action} com sucesso.")
 
 def main():
-    file_name = "FURGSfs2.fs"
-
     while True:
-        total_size_mb = int(input("Digite o tamanho do sistema de arquivos em MB (entre 10 e 200): "))
-        if 10 <= total_size_mb <= 200:
+        tamanhoTotalMb = int(input("Digite o tamanho do sistema de arquivos em MB (entre 10 e 200): "))
+        if 10 <= tamanhoTotalMb <= 200:
             break
         print("Por favor, insira um valor válido entre 10 e 200 MB.")
 
-    total_size = total_size_mb * 1024 * 1024  # Converter MB para bytes
+    tamanhoTotal = tamanhoTotalMb * 1024 * 1024  # Converter MB para bytes
 
-    fs = SimpleFATFileSystem(file_name, total_size)
+    sistemaArquivos = FURGfs2(tamanhoTotal)
 
     while True:
         print("\nOpções:")
@@ -230,31 +228,31 @@ def main():
         print("7. Proteger ou desproteger um arquivo")
         print("8. Sair")
 
-        choice = input("Escolha uma opção: ")
+        opcao = input("Escolha uma opção: ")
 
-        if choice == '1':
-            source_path = input("Digite o caminho do arquivo no disco: ")
-            fs.copy_to_fs(source_path)
-        elif choice == '2':
-            file_name = input("Digite o nome do arquivo no sistema de arquivos: ")
-            dest_path = input("Digite o caminho de destino no disco: ")
-            fs.copy_from_fs(file_name, dest_path)
-        elif choice == '3':
-            old_name = input("Digite o nome atual do arquivo: ")
-            new_name = input("Digite o novo nome do arquivo: ")
-            fs.rename_file(old_name, new_name)
-        elif choice == '4':
-            file_name = input("Digite o nome do arquivo a ser removido: ")
-            fs.remove_file(file_name)
-        elif choice == '5':
-            fs.list_files()
-        elif choice == '6':
-            fs.get_free_space()
-        elif choice == '7':
-            file_name = input("Digite o nome do arquivo: ")
-            protect = input("Proteger (s/n)? ").lower() == 's'
-            fs.protect_file(file_name, protect)
-        elif choice == '8':
+        if opcao == '1':
+            enderecoArquivoDisco = input("Digite o caminho do arquivo no disco: ")
+            sistemaArquivos.copiarArquivoDiscoSistema(enderecoArquivoDisco)
+        elif opcao == '2':
+            nomeArquivoDisco = input("Digite o nome do arquivo no sistema de arquivos: ")
+            enderecoDestino = input("Digite o caminho de destino no disco: ")
+            sistemaArquivos.copiarArquivoSistemaDisco(nomeArquivoDisco, enderecoDestino)
+        elif opcao == '3':
+            nomeAntigo = input("Digite o nome atual do arquivo: ")
+            nomeNovo = input("Digite o novo nome do arquivo: ")
+            sistemaArquivos.renomearArquivo(nomeAntigo, nomeNovo)
+        elif opcao == '4':
+            nomeArquivoSistema = input("Digite o nome do arquivo a ser removido: ")
+            sistemaArquivos.deletarArquivo(nomeArquivoSistema)
+        elif opcao == '5':
+            sistemaArquivos.informarArquivosSistema()
+        elif opcao == '6':
+            sistemaArquivos.informarEspacoDisponivel()
+        elif opcao == '7':
+            nomeArquivoSistema = input("Digite o nome do arquivo: ")
+            protegido = input("Proteger (s/n)? ").lower() == 's'
+            sistemaArquivos.protegerArquivo(nomeArquivoSistema, protegido)
+        elif opcao == '8':
             break
         else:
             print("Opção inválida.")
